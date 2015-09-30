f6d19934d076569fdaf785e22e4d009a68b742e3
zaps166 <spaz16@wp.pl>
2015-10-01 00:21:46 +0200

    Update to 1.1.0

--- README.md
+++ README.md
@@ -4,9 +4,11 @@
 
 ###Known bugs and limitations:
 - this applet uses internal "xfce4-panel" and "mate-panel" structures, so it may be incompatible between versions,
+- crash is possible after removing some Xfce4 panel plugins like "xfce4-clipman-plugin" (sometimes after a while),
+- about context menu doesn't exists due to problems when more then one Xfce4 panel plugin is loaded,
+- bug in Xfce4 panel plugin can crash all Xfce4 panel plugins loaded into Mate panel,
 - adding item to panel from context menu in "xfce4-whiskermenu-plugin" doesn't work,
-- about context menu doesn't work correctly when more then one applet is loaded,
-- "xfce4-whiskermenu-plugin" can crash when more then one instance is created.
+- cannot load more then one the same Xfce4 panel plugin,
 
 ###MATE commands for Whisker Menu:
 - All Settings: "mate-control-center",
--- src/mate-xfce4-panel-plugin-loader-applet.c
+++ src/mate-xfce4-panel-plugin-loader-applet.c
@@ -33,6 +33,7 @@
 
 #define PANEL_SETTINGS_SCHEMA "org.mate.panel.applet.Xfce4PanelPluginLoader"
 #define PANEL_SETTINGS_KEY_FILENAME_SCHEMA "filename"
+#define XFCE4_ICON "xfce4-logo"
 
 /* Xfce4 plugin private struct from "xfce4-panel" source code */
 struct _XfcePanelPluginPrivate
@@ -102,11 +103,16 @@ struct _MatePanelAppletPrivate
 	gboolean           locked_down;
 };
 
+/* Global array */
+static int num_applets = 0;
+static gchar **xfce4_panel_plugin_filenames = NULL;
+
 /* Structure */
 typedef struct
 {
 	GSettings *settings;
 	GtkWidget *button;
+	int lib_name_pos;
 	MatePanelApplet *mate_applet;
 	XfcePanelPlugin *xfce_panel_plugin;
 } Xfce4PanelPluginLoader;
@@ -133,7 +139,7 @@ static void xfce4_panel_plugin_loader_handle_size_change(gpointer mate_applet, g
 	else if (xfce4_panel_plugin_loader->button)
 	{
 		GtkWidget *image = gtk_image_new();
-		GdkPixbuf *icon = xfce_panel_pixbuf_from_source_at_size("xfce4-logo", gtk_icon_theme_get_default(), size, size);
+		GdkPixbuf *icon = xfce_panel_pixbuf_from_source_at_size(XFCE4_ICON, gtk_icon_theme_get_default(), size, size);
 		gtk_image_set_from_pixbuf(GTK_IMAGE(image), icon);
 		g_object_unref(G_OBJECT(icon));
 		gtk_button_set_image(GTK_BUTTON(xfce4_panel_plugin_loader->button), image);
@@ -142,14 +148,38 @@ static void xfce4_panel_plugin_loader_handle_size_change(gpointer mate_applet, g
 static void xfce4_panel_plugin_loader_handle_dispose(MatePanelApplet *mate_applet, Xfce4PanelPluginLoader *xfce4_panel_plugin_loader)
 {
 	(void)mate_applet;
-	/* "xfce4_panel_plugin_loader->xfce_panel_plugin" - automatically destroys itself with "mate_applet"*/
+	if (xfce4_panel_plugin_loader->xfce_panel_plugin)
+	{
+		/* Free information about the loaded library */
+		g_free(xfce4_panel_plugin_filenames[xfce4_panel_plugin_loader->lib_name_pos]);
+		xfce4_panel_plugin_filenames[xfce4_panel_plugin_loader->lib_name_pos] = NULL;
+
+		/* "xfce4_panel_plugin_loader->xfce_panel_plugin" - automatically destroys itself with "mate_applet" */
+		xfce4_panel_plugin_loader->xfce_panel_plugin = NULL;
+	}
 	/* "lib_handle" - unloading libraries is dangerous and causes crashes */
-	xfce4_panel_plugin_loader->xfce_panel_plugin = NULL;
 	if (xfce4_panel_plugin_loader->button)
 		gtk_widget_destroy(GTK_WIDGET(xfce4_panel_plugin_loader->button));
 	if (xfce4_panel_plugin_loader->settings)
 		g_object_unref(xfce4_panel_plugin_loader->settings);
 	g_free(xfce4_panel_plugin_loader);
+
+	/* Free global array if can */
+	gboolean can_free = TRUE;
+	int i;
+	for (i = 0; i < num_applets; ++i)
+	{
+		if (xfce4_panel_plugin_filenames[i] != NULL)
+		{
+			can_free = FALSE;
+			break;
+		}
+	}
+	if (can_free)
+	{
+		g_free(xfce4_panel_plugin_filenames);
+		xfce4_panel_plugin_filenames = NULL;
+	}
 }
 
 /* Helper function for creating menu separator */
@@ -207,14 +237,38 @@ static gchar *get_lib_file_name()
 	return file_name;
 }
 
-/* Load Xfce4 applet */
-static XfcePanelPlugin *get_xfce_panel_plugin(void **handle, const gchar *file_name)
+/* Helper function to show message dialogs */
+static void show_message(const gchar *lib_name, const gchar *info_msg)
 {
-	*handle = dlopen(file_name, RTLD_LOCAL | RTLD_LAZY);
-	if (*handle)
+	gchar *msg = (gchar *)g_malloc(strlen(lib_name) + 2 + strlen(info_msg) + 1);
+	strcpy(msg, lib_name);
+	strcat(msg, ": ");
+	strcat(msg, info_msg);
+
+	GtkWidget *dlg = gtk_message_dialog_new(NULL, 0, GTK_MESSAGE_INFO, GTK_BUTTONS_OK, msg);
+	gtk_dialog_run(GTK_DIALOG(dlg));
+	gtk_widget_destroy(dlg);
+
+	g_free(msg);
+}
+
+/* Load Xfce4 applet */
+static XfcePanelPlugin *get_xfce_panel_plugin(const gchar *file_name)
+{
+	void *handle = dlopen(file_name, RTLD_LOCAL | RTLD_LAZY);
+	if (!handle)
+		show_message(file_name, _("cannot load the library!"));
+	else
 	{
-		PluginConstructFunc construct_func = dlsym(*handle, "xfce_panel_module_construct");
-		if (construct_func)
+		PluginConstructFunc construct_func = dlsym(handle, "xfce_panel_module_construct");
+		if (!construct_func)
+		{
+			if (dlsym(handle, "xfce_panel_module_init"))
+				show_message(file_name, _("only external Xfce4 panel plugins are supported!"));
+			else
+				show_message(file_name, _("is not Xfce4 panel plugin library!"));
+		}
+		else
 		{
 			const gchar *lib_name = strrchr(file_name, '/');
 			size_t lib_name_len = strlen(lib_name);
@@ -230,16 +284,34 @@ static XfcePanelPlugin *get_xfce_panel_plugin(void **handle, const gchar *file_n
 				return xfce_panel_plugin;
 			}
 		}
-		dlclose(*handle);
-		*handle = NULL;
+		dlclose(handle);
 	}
 	return NULL;
 }
 static gboolean load_xfce4_panel_plugin(Xfce4PanelPluginLoader *xfce4_panel_plugin_loader, gchar *lib_file_name)
 {
+	/* Extract library file name from path */
+	const gchar *lib_name = strrchr(lib_file_name, '/');
+	if (lib_name)
+		++lib_name;
+	else
+		lib_name = lib_file_name;
+
+	/* Check whether Xfce4 panel plugin is already loaded */
+	int i;
+	for (i = 0; i < num_applets; ++i)
+	{
+		if (xfce4_panel_plugin_filenames[i] == NULL)
+			continue;
+		if (!strcmp(xfce4_panel_plugin_filenames[i], lib_name))
+		{
+			show_message(lib_name, _("the library is already loaded!"));
+			return FALSE;
+		}
+	}
+
 	/* Load Xfce4 panel plugin library */
-	void *lib_handle;
-	XfcePanelPlugin *xfce_panel_plugin = get_xfce_panel_plugin(&lib_handle, lib_file_name);
+	XfcePanelPlugin *xfce_panel_plugin = get_xfce_panel_plugin(lib_file_name);
 	if (!xfce_panel_plugin)
 	{
 		g_settings_set_string(xfce4_panel_plugin_loader->settings, PANEL_SETTINGS_KEY_FILENAME_SCHEMA, "");
@@ -284,14 +356,9 @@ static gboolean load_xfce4_panel_plugin(Xfce4PanelPluginLoader *xfce4_panel_plug
 		gtk_widget_show(item);
 	}
 
-	/* Add "about" item to the menu */
-	if (xfce_panel_plugin_provider_get_show_about((XfcePanelPluginProvider *)xfce_panel_plugin))
-	{
-		GtkWidget *item = gtk_image_menu_item_new_from_stock(GTK_STOCK_ABOUT, NULL);
-		g_signal_connect_swapped(G_OBJECT(item), "activate", G_CALLBACK(xfce_panel_plugin_provider_show_about), xfce_panel_plugin);
-		gtk_menu_shell_append(GTK_MENU_SHELL(xfce_panel_plugin->priv->menu), item);
-		gtk_widget_show(item);
-	}
+	/*
+	 * Don't add "about" item to the menu, because it is not wotking properly when more then one Xfce4 panel plugin is loaded
+	*/
 
 	/* Add other plugin items to the menu */
 	GSList *menu_items = xfce_panel_plugin->priv->menu_items;
@@ -316,6 +383,15 @@ static gboolean load_xfce4_panel_plugin(Xfce4PanelPluginLoader *xfce4_panel_plug
 	/* Set size */
 	xfce4_panel_plugin_loader_handle_size_change(NULL, mate_panel_applet_get_size(xfce4_panel_plugin_loader->mate_applet), xfce4_panel_plugin_loader);
 
+	/* Reset translations (Xfce4 panel plugin probably set it to its own package) */
+	textdomain(GETTEXT_PACKAGE);
+
+	/* Add to global array that the library is loaded */
+	xfce4_panel_plugin_filenames = (gchar **)g_realloc(xfce4_panel_plugin_filenames, ++num_applets * sizeof(void *) );
+	xfce4_panel_plugin_filenames[num_applets-1] = (gchar *)g_malloc(strlen(lib_name)+1);
+	strcpy(xfce4_panel_plugin_filenames[num_applets-1], lib_name);
+	xfce4_panel_plugin_loader->lib_name_pos = num_applets-1;
+
 	return TRUE;
 }
 
@@ -325,9 +401,9 @@ static void file_dialog_button_clicked(GtkWidget *button, Xfce4PanelPluginLoader
 	(void)button;
 	gtk_widget_set_sensitive(GTK_WIDGET(xfce4_panel_plugin_loader->button), FALSE);
 	gchar *lib_file_name = get_lib_file_name();
-	gtk_widget_set_sensitive(GTK_WIDGET(xfce4_panel_plugin_loader->button), TRUE);
 	if (lib_file_name && *lib_file_name)
 		load_xfce4_panel_plugin(xfce4_panel_plugin_loader, lib_file_name);
+	gtk_widget_set_sensitive(GTK_WIDGET(xfce4_panel_plugin_loader->button), TRUE);
 }
 static gboolean do_not_eat_button_press(GtkWidget *widget, GdkEventButton *event)
 {
@@ -347,6 +423,89 @@ static void create_file_dialog_button(Xfce4PanelPluginLoader *xfce4_panel_plugin
 	gtk_widget_show_all(GTK_WIDGET(xfce4_panel_plugin_loader->mate_applet));
 }
 
+/* Helper function to set environmental variables */
+static FILE *open_proc_environ(const char *pname_to_find)
+{
+	/* Open "/proc" directory */
+	DIR *dirp = opendir("/proc");
+	if (!dirp)
+		return NULL;
+
+	/* Scan "/proc" dir */
+	struct dirent *dp;
+	while ((dp = readdir(dirp)) != NULL)
+	{
+		/* Convert dir name to number */
+		char *end_ptr;
+		pid_t pid = strtol(dp->d_name, &end_ptr, 10);
+		/* Directory name is not entirely numeric */
+		if (*end_ptr != '\0')
+			continue;
+
+		/* Open the "stat" file */
+		char pth[32];
+		snprintf(pth, sizeof(pth), "/proc/%d/comm", pid);
+		FILE *f = fopen(pth, "r");
+		if (f)
+		{
+			/* Get process name from file and check whether is the same as in function argument */
+			char pname[256];
+			if ((fscanf(f, "%255s", pname)) == 1 && !strcmp(pname, pname_to_find))
+			{
+				fclose(f);
+				closedir(dirp);
+				/* Open "environ" file */
+				snprintf(pth, sizeof pth, "/proc/%d/environ", pid);
+				return fopen(pth, "rb");
+			}
+			fclose(f);
+		}
+	}
+
+	closedir(dirp);
+	return NULL;
+}
+static void set_environmental_variables_from_mate_panel()
+{
+	FILE *f = open_proc_environ("mate-panel");
+	if (f)
+	{
+		int env_buff_size = 0, env_buff_pos = 0;
+		char *env_buff = NULL;
+		for (;;)
+		{
+			/* Read one byte */
+			int c = fgetc(f);
+			/* EOF or error */
+			if (c < 0)
+				break;
+			/* Expand buffer */
+			if (env_buff_pos == env_buff_size)
+				env_buff = (char *)realloc(env_buff, ++env_buff_size);
+			/* Append byte to buffer */
+			env_buff[env_buff_pos++] = c;
+			/* End of single entry */
+			if (c == '\0')
+			{
+				/* Allocate more memroy than necessary */
+				char *var = (char *)malloc(env_buff_pos);
+				char *val = (char *)malloc(env_buff_pos);
+				/* Get variable name and its value */
+				sscanf(env_buff, "%[^=]=%s", var, val);
+				/* Set env */
+				setenv(var, val, 1);
+				/* Free allocated memory */
+				free(var);
+				free(val);
+				/* Reset buffer position */
+				env_buff_pos = 0;
+			}
+		}
+		free(env_buff);
+		fclose(f);
+	}
+}
+
 /* Load applet */
 static gboolean xfce4_panel_plugin_loader_factory(MatePanelApplet *mate_applet, const gchar *iid, gpointer data)
 {
@@ -357,7 +516,7 @@ static gboolean xfce4_panel_plugin_loader_factory(MatePanelApplet *mate_applet, 
 		xfce4_panel_plugin_loader->settings = mate_panel_applet_settings_new(mate_applet, PANEL_SETTINGS_SCHEMA);
 		xfce4_panel_plugin_loader->mate_applet = mate_applet;
 
-		/* Handle dispose signal */
+		/* Handle destroy signal */
 		g_signal_connect(G_OBJECT(mate_applet), "destroy", G_CALLBACK(xfce4_panel_plugin_loader_handle_dispose), xfce4_panel_plugin_loader);
 
 		/* Handle change size */
@@ -366,6 +525,10 @@ static gboolean xfce4_panel_plugin_loader_factory(MatePanelApplet *mate_applet, 
 		/* Set Mate applet options */
 		mate_panel_applet_set_flags(mate_applet, MATE_PANEL_APPLET_EXPAND_MINOR);
 		mate_panel_applet_set_background_widget(mate_applet, GTK_WIDGET(mate_applet));
+
+		/* Set environmental variables from "mate-panel" (Linux only), call only once */
+		if (num_applets == 0)
+			set_environmental_variables_from_mate_panel();
 
 		/* Get library file name from settings */
 		gchar *lib_file_name = g_settings_get_string(xfce4_panel_plugin_loader->settings, PANEL_SETTINGS_KEY_FILENAME_SCHEMA);
@@ -376,6 +539,8 @@ static gboolean xfce4_panel_plugin_loader_factory(MatePanelApplet *mate_applet, 
 
 		if (lib_file_name)
 			g_free(lib_file_name);
+
+		gtk_window_set_default_icon_name(XFCE4_ICON);
 
 		return TRUE;
 	}
